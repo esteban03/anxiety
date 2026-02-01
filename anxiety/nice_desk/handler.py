414990e6fa706a31fcaac0c2c444eddcc5a8a99f
estebansanchez <esteban03@outlook.cl>
2026-02-01 10:54:46 -0300

    refactor: move code to a class

--- anxiety/nice_desk/handler.py
+++ anxiety/nice_desk/handler.py
@@ -1,33 +1,13 @@
-import shutil
 import time
-from pathlib import Path
 
 from watchdog.events import FileSystemEventHandler, FileSystemEvent, DirCreatedEvent
+
+from anxiety.nice_desk.main import NiceDesk
 
 
 class NiceDeskHandler(FileSystemEventHandler):
     def on_created(self, event: FileSystemEvent | DirCreatedEvent):
         time.sleep(0.5)
 
-        path = Path(event.src_path)
-
-        if path.parent.name != "Desktop":
-            return
-
-        if path.parent.parent.name == "Desktop":
-            return
-
-        if path.is_dir():
-            return
-
-        download_folder = Path("~/Desktop").expanduser()
-
-        target_folder = download_folder / "Desktop"
-        target_folder.mkdir(exist_ok=True)
-
-        for file in download_folder.iterdir():
-            if file.is_dir():
-                continue
-
-            if file.exists():
-                shutil.move(str(file), str(target_folder / file.name))
+        nice_desk = NiceDesk(event.src_path)
+        nice_desk.run()
--- anxiety/nice_desk/main.py
+++ anxiety/nice_desk/main.py
@@ -1,1 +1,36 @@
+import shutil
+from pathlib import Path
 
+
+class NiceDesk:
+
+    def __init__(self, src_path: str | Path) -> None:
+        self.src_path = Path(src_path)
+        self.download_folder = Path("~/Desktop").expanduser()
+        self.target_folder = self.download_folder / "Desktop"
+
+    def _skip(self) -> bool:
+        path = self.src_path
+
+        if path.parent.name != "Desktop":
+            return True
+
+        if path.parent.parent.name == "Desktop":
+            return True
+
+        if path.is_dir():
+            return True
+
+        return False
+
+    def run(self) -> None:
+        if self._skip():
+            return
+
+        self.target_folder.mkdir(exist_ok=True)
+
+        for file in self.download_folder.iterdir():
+            if file.is_dir() or not file.exists():
+                continue
+
+            shutil.move(str(file), str(self.target_folder / file.name))
